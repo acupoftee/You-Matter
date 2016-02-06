40982e5959b38dd51d0cfb5f819438a75e8e41c7
Bethy Diakabana <bethyd123@gmail.com>
2016-02-05 23:23:34 -0500

    Update bot.py

--- bot.py
+++ bot.py
@@ -47,7 +47,8 @@ data = {'like':
 filters = [
     'http', '#nowplaying', 'youtube', '-', '"', u'“', u'”', 'poor boy',
     'the way you do', 'hear it every day', 'that\'s ok', 'thats ok',
-    'nobody loves me better', 'better than you'
+    'nobody loves me better', 'better than you', 'you\'re so fine',
+    '*'
     ]
 
 
